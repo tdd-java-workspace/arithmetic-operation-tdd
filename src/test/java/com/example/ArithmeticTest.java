cba283c6b28ca599d1c4851893878abbb76105e9
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-29 09:25:44 +0530

    UPDATE. Added suits for the tests.

--- src/test/java/com/example/ArithmeticTest.java
+++ src/test/java/com/example/ArithmeticTest.java
@@ -1,57 +1,64 @@
 package com.example;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArithmeticTest {
 
-    @Test
-    void toReturnFiveWhenThreeIsAddedWithTwo() {
+    @Nested
+    class Addition {
+        @Test
+        void toReturnFiveWhenThreeIsAddedWithTwo() {
 
 
-//        AAA
-//        Arrange
-        Arithmetic arithmetic = new Arithmetic();
-        int two = 2, three = 3, five = 5;
+    //        AAA
+    //        Arrange
+            Arithmetic arithmetic = new Arithmetic();
+            int two = 2, three = 3, five = 5;
 
-//        Act
-        int actualSum = arithmetic.add(three, two);
+    //        Act
+            int actualSum = arithmetic.add(three, two);
 
-//        Assert
-        assertEquals(five, actualSum);
+    //        Assert
+            assertEquals(five, actualSum);
+        }
+
+        @Test
+        void toReturnThreeWhenFiveIsAddedWithMinusTwo() {
+
+            Arithmetic arithmetic = new Arithmetic();
+            int minusTwo = -2, three = 3, five = 5;
+
+            int actualSum = arithmetic.add(five, minusTwo);
+
+            assertEquals(three, actualSum);
+        }
+
+        @Test
+        void toReturnThreeWhenThreeIsAddedWithZero() {
+
+            Arithmetic arithmetic = new Arithmetic();
+            int zero = 0, three = 3;
+
+            int actualSum = arithmetic.add(three, zero);
+
+            assertEquals(three, actualSum);
+        }
     }
 
-    @Test
-    void toReturnThreeWhenFiveIsAddedWithMinusTwo() {
+    @Nested
+    class Subtraction {
+        @Test
+        void toReturnFiveWhenSevenIsSubtractedWithTwo() {
 
-        Arithmetic arithmetic = new Arithmetic();
-        int minusTwo = -2, three = 3, five = 5;
+            Arithmetic arithmetic = new Arithmetic();
+            int two = 2, five = 5, seven = 7;
 
-        int actualSum = arithmetic.add(five, minusTwo);
+            int actualDifference = arithmetic.sub(seven, two);
 
-        assertEquals(three, actualSum);
-    }
-
-    @Test
-    void toReturnThreeWhenThreeIsAddedWithZero() {
-
-        Arithmetic arithmetic = new Arithmetic();
-        int zero = 0, three = 3;
-
-        int actualSum = arithmetic.add(three, zero);
-
-        assertEquals(three, actualSum);
-    }
-
-    @Test
-    void toReturnFiveWhenSevenIsSubtractedWithTwo() {
-
-        Arithmetic arithmetic = new Arithmetic();
-        int two = 2, five = 5, seven = 7;
-
-        int actualSum = arithmetic.sub(seven, two);
-
-        assertEquals(five, actualSum);
+            assertEquals(five, actualDifference);
+        }
     }
 }
