19d425bb09994274e38908aad09be7c6e0f6e1c4
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-29 09:05:09 +0530

    Initial commit.

---
+++ src/test/java/com/example/ArithmeticTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class ArithmeticTest {
+
+    @Test
+    void toReturnFiveWhenThreeIsAddedWithTwo() {
+
+        Arithmetic arithmetic = new Arithmetic();
+    }
+}
