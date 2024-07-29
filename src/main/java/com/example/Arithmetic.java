2d6e029006413e193dd13c10ee6907e63474d280
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-29 09:07:41 +0530

    ADD. First test, to check for addition of two numbers.

--- src/main/java/com/example/Arithmetic.java
+++ src/main/java/com/example/Arithmetic.java
@@ -2,4 +2,7 @@ package com.example;
 
 public class Arithmetic {
 
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
 }
--- src/test/java/com/example/ArithmeticTest.java
+++ src/test/java/com/example/ArithmeticTest.java
@@ -2,11 +2,15 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticTest {
 
     @Test
     void toReturnFiveWhenThreeIsAddedWithTwo() {
 
         Arithmetic arithmetic = new Arithmetic();
+
+        assertEquals(5, arithmetic.add(3, 2));
     }
 }
